90bcdf9eb31b37ddf04d3eb538b2a8071b901557
Don Naegely <naegelyd@gmail.com>
2013-11-19 13:14:59 -0500

    Add test data and fix the expected values in sample_load_process tests
    All existing tests should be working as of this commit.

--- tests/cases/sample_load_process/tests.py
+++ tests/cases/sample_load_process/tests.py
@@ -48,23 +48,44 @@ class SampleLoadTestCase(QueueTestCase):
         worker1 = get_worker('variants')
         worker2 = get_worker('default')
 
+        expected_counts = {
+            'batches': 3,
+            'cohorts': 2,
+            'genes': 193,
+            'projects': 1,
+            'results_per_sample': [(1, 1963), (2, 1963), (3, 1963), (4, 2094),
+                                   (5, 2094), (6, 2094), (7, 4377), (8, 4377),
+                                   (9, 4377)],
+            'samples': 9,
+            'transcripts': 351,
+            'variant_effects': 15698,
+            'variants': 8434,
+            'samples_per_batch': [(1, 3), (2, 3), (3, 3)],
+        }
+        expected_counts['results'] = \
+            sum([count for pk, count in expected_counts['results_per_sample']])
+
         # Ensure sample-related entries are created..
-        self.assertEqual(Project.objects.count(), 1)
-        self.assertEqual(Batch.objects.count(), 4)
-        self.assertEqual(Sample.objects.count(), 15)
+        self.assertEqual(Project.objects.count(), expected_counts['projects'])
+        self.assertEqual(Batch.objects.count(), expected_counts['batches'])
+        self.assertEqual(Sample.objects.count(), expected_counts['samples'])
 
         # World and project cohort..
-        self.assertEqual(Cohort.objects.count(), 2)
+        self.assertEqual(Cohort.objects.count(), expected_counts['cohorts'])
 
         # Nothing published yet..
-        self.assertEqual(Sample.objects.filter(published=False).count(), 15)
+        self.assertEqual(Sample.objects.filter(published=False).count(),
+                         expected_counts['samples'])
         self.assertEqual(
-            Cohort.objects.filter(count=0, published=False).count(), 2)
+            Cohort.objects.filter(count=0, published=False).count(),
+            expected_counts['cohorts'])
         self.assertEqual(
-            Batch.objects.filter(count=0, published=False).count(), 4)
+            Batch.objects.filter(count=0, published=False).count(),
+            expected_counts['batches'])
 
         # Manifests are stored
-        self.assertEqual(SampleManifest.objects.count(), 15)
+        self.assertEqual(SampleManifest.objects.count(),
+                         expected_counts['samples'])
         for manifest in SampleManifest.objects.all():
             self.assertNotEqual(manifest.content, '')
             self.assertFalse(manifest.content_has_changed())
@@ -72,14 +93,16 @@ class SampleLoadTestCase(QueueTestCase):
         # Work on variants...
         worker1.work(burst=True)
 
-        self.assertEqual(Variant.objects.count(), 674)
+        self.assertEqual(Variant.objects.count(), expected_counts['variants'])
 
         # Work on effects...
         worker2.work(burst=True)
 
-        self.assertEqual(Gene.objects.count(), 104)
-        self.assertEqual(Transcript.objects.count(), 255)
-        self.assertEqual(VariantEffect.objects.count(), 1418)
+        self.assertEqual(Gene.objects.count(), expected_counts['genes'])
+        self.assertEqual(Transcript.objects.count(),
+                         expected_counts['transcripts'])
+        self.assertEqual(VariantEffect.objects.count(),
+                         expected_counts['variant_effects'])
 
         self.assertEqual(Sift.objects.count(), 0)
         self.assertEqual(PolyPhen2.objects.count(), 0)
@@ -87,21 +110,20 @@ class SampleLoadTestCase(QueueTestCase):
         self.assertEqual(EVS.objects.count(), 0)
 
         # Results loaded..
-        self.assertEqual(Result.objects.count(), 3436)
+        self.assertEqual(Result.objects.count(), expected_counts['results'])
 
         # Batches are now published..
-        self.assertEqual(Batch.objects.filter(published=True).count(), 3)
+        self.assertEqual(Batch.objects.filter(published=True).count(),
+                         expected_counts['batches'])
 
         # Ensure the counts are accurate for each sample..
-        for pk, count in [(1, 289), (2, 281), (3, 268), (4, 295), (5, 296),
-                          (6, 293), (7, 264), (8, 289), (9, 264), (10, 293),
-                          (11, 289), (12, 315)]:
+        for pk, count in expected_counts['results_per_sample']:
             sample = Sample.objects.get(pk=pk)
             self.assertTrue(sample.published)
             self.assertEqual(sample.count, count)
 
         # Batches are created with the samples, but are unpublished
-        for pk, count in [(1, 5), (2, 3), (3, 4)]:
+        for pk, count in expected_counts['samples_per_batch']:
             batch = Batch.objects.get(pk=pk)
             self.assertTrue(batch.published)
             self.assertEqual(batch.count, count)
@@ -115,7 +137,7 @@ class SnpeffReloadTest(QueueTestCase):
     def test(self):
         "Load a single VCF, reload the snpEff data using the same VCF."
         management.call_command('samples', 'queue',
-                                os.path.join(SAMPLE_DIRS[0], 'batch1/sample1'))
+                                os.path.join(SAMPLE_DIRS[0], 'batch1/locus_1'))
 
         # Synchronously work on queue
         worker1 = get_worker('variants')
@@ -123,15 +145,24 @@ class SnpeffReloadTest(QueueTestCase):
         worker1.work(burst=True)
         worker2.work(burst=True)
 
-        self.assertEqual(VariantEffect.objects.count(), 614)
+        expected_variant_effects_count = 5426
+
+        self.assertEqual(VariantEffect.objects.count(),
+                         expected_variant_effects_count)
         self.assertEqual(
-            VariantEffect.objects.aggregate(max_id=Max('id'))['max_id'], 614)
+            VariantEffect.objects.aggregate(max_id=Max('id'))['max_id'],
+            expected_variant_effects_count)
 
         management.call_command('variants', 'reload-snpeff',
                                 os.path.join(SAMPLE_DIRS[0],
-                                             'batch1/sample1/results.vcf'))
+                                             'batch1/locus_1/locus_1.vcf'))
 
         # Ensure data was actually reloaded, check the auto-incremented key
-        self.assertEqual(VariantEffect.objects.count(), 614)
-        self.assertEqual(VariantEffect.objects.aggregate(
-            max_id=Max('id'))['max_id'], 614 * 2)
+        self.assertEqual(VariantEffect.objects.count(),
+                         expected_variant_effects_count)
+
+        # Since we reloaded, we should now have double the number of expected
+        # result, thus the 2 * operation in the assertion below.
+        self.assertEqual(
+            VariantEffect.objects.aggregate(max_id=Max('id'))['max_id'],
+            2 * expected_variant_effects_count)
