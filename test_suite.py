150671c9f6d55ed9036ec5aa3c6200c104744d56
Don Naegely <naegelyd@gmail.com>
2013-12-17 15:19:38 -0500

    Allow for specifying specific tests in test_suite.py

--- test_suite.py
+++ test_suite.py
@@ -1,8 +1,18 @@
-#! /usr/bin/env python
 import os
+import sys
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
 from django.core import management
-management.call_command('test', 'sample_load_process', 'south_tests',
-                        'geneset_form', 'commands')
+
+apps = sys.argv[1:]
+
+if not apps:
+    apps = [
+        'sample_load_process',
+        'south_tests',
+        'geneset_form',
+        'commands',
+    ]
+
+management.call_command('test', *apps)
