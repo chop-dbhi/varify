828adc4dcd127475d40c08f917e412ead4cbd6d9
Don Naegely <naegelyd@gmail.com>
2013-12-20 16:27:28 -0500

    Remove Cilantro references from varify.views

--- varify/views.py
+++ varify/views.py
@@ -1,45 +1,6 @@
-import json
 from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm
 from news.models import Article
-from guardian.shortcuts import get_objects_for_user
-from cilantro.models import UserPreferences
-from varify.samples.models import Sample
-
-
-def app(request, project=None, batch=None, sample=None):
-    if hasattr(request, 'user') and request.user.is_authenticated():
-        kwargs = {'user': request.user}
-    else:
-        kwargs = {'session_key': request.session.session_key}
-
-    obj, created = UserPreferences.objects.get_or_create(**kwargs)
-    preferences = obj.json
-    preferences['id'] = obj.pk
-
-    selectedProband = {}
-    if project and batch:
-        selectedProband['project'] = project
-        selectedProband['batch'] = batch
-        selectedProband['sample'] = sample
-
-    projects = get_objects_for_user(request.user, 'samples.view_project')
-
-    queryset = Sample.objects.select_related('batch', 'project')\
-        .filter(published=True, batch__published=True, project__in=projects)\
-        .values_list('pk', 'label', 'batch__name', 'project__name')\
-        .order_by('project', 'batch', 'label')
-
-    samples = []
-    keys = ['id', 'sample', 'batch', 'project']
-    for row in queryset:
-        samples.append(dict(zip(keys, row)))
-
-    return render(request, 'cilantro/index.html', {
-        'user_preferences': json.dumps(preferences),
-        'samples': json.dumps(samples),
-        'selected_proband': json.dumps(selectedProband),
-    })
 
 
 def index(request):
