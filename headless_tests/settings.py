f3b75d98282b86e9316768a3ddd9e48cf89afa5a
Le Mar Davidson <davidsonl2@email.chop.edu>
2014-01-02 13:24:58 -0500

    Adding sample data for headless test

--- headless_tests/settings.py
+++ headless_tests/settings.py
@@ -1,12 +1,12 @@
-#from global_settings import LDAP, INSTALLED_APPS
+from global_settings import  INSTALLED_APPS
+import os, sys
 
 # Uncomment to put the application in non-debug mode. This is useful
 # for testing error handling and messages.
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 # Override this to match the application endpoint
-
 
 DATABASES = {
     'default': {
@@ -25,9 +25,6 @@ EMAIL_SUBJECT_PREFIX = '[Varify Headless Testing] '
 # This is used as a "seed" for various hashing algorithms. This must be set to
 # a very long random string (40+ characters)
 SECRET_KEY = '1234536yskkshkka9937745204981921jhkkakis992763yhahha'
-
-
-SITE_ID = 2
 
 
 # Sentry is collating the error messages..
@@ -52,7 +49,65 @@ RQ_QUEUES = {
         'DB': 0,
     },
 }
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(os.path.abspath(os.path.dirname(__name__)),'headless_tests/test/log/varify.log'),
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'avocado': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'serrano': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'varify': {
+            'handlers': ['console','file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        './varify/samples/management/subcommands/queue.pyc': {
+	    'handlers': ['console','file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'rq.worker': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
             
 VARIFY_SAMPLE_DIRS = (
-    '/home/vagrant/webapps/varify-os/samples/',
+    os.path.join(os.path.abspath(os.path.dirname(__name__)),'headless_tests/samples'),
 )
