4a91e12ee9b32c27aeef7652116f4312a016f564
Don Naegely <naegelyd@gmail.com>
2014-02-06 11:08:40 -0500

    Update PyVCF requirement to >=0.6.5
    This is to account for the new PyVCF version released to pypi today.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ install_requires = [
     'pycap==0.8.1',
     'django-reversion==1.6.6',
     'diff-match-patch',
-    'vcf==0.6.5',
+    'pyvcf>=0.6.5',
 ]
 
 kwargs = {
@@ -43,13 +43,6 @@ kwargs = {
                                        'tests.*']),
     'include_package_data': True,
     'install_requires': install_requires,
-    # This is a hack to get setuptools to install the the latest version of
-    # PyVCF from the github repo. This version doesn't yet exist on pypi so the
-    # so the entry install_requires won't find anything and it will fall back
-    # to this github link.
-    'dependency_links': [
-        'https://github.com/jamescasbon/PyVCF/archive/master.zip#egg=vcf-0.6.5',
-    ],
     'test_suite': 'test_suite',
     'author': '',
     'author_email': '',
