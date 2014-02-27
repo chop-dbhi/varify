71dfed802e9272d1d1d96b566431fe2f430b23ec
Byron Ruth <b@devel.io>
2014-02-27 13:03:55 -0500

    Flake 8 fixes

--- varify/conf/global_settings.py
+++ varify/conf/global_settings.py
@@ -1,11 +1,12 @@
 import os
 
 # Import global settings to make it easier to extend settings.
-from django.conf.global_settings import *
+from django.conf.global_settings import *  # noqa
 
 # Import the project module to calculate directories relative to the module
 # location.
-PROJECT_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../..')
+PROJECT_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                            '../..')
 
 # List all Django apps here. Note that standard Python libraries should not
 # be added to this list since Django will not recognize them as apps anyway.
@@ -89,8 +90,8 @@ DATABASES = {}
 # to different databases depending on what data is being acted on. For Harvest
 # instances that make use of an existing database, it is typically never
 # desirable to create all the Harvest application tables in this database, but
-# rather have a separate database for this purpose. That way the "data" database
-# does not need to be changed.
+# rather have a separate database for this purpose. That way the "data"
+# database does not need to be changed.
 
 DATABASE_ROUTERS = {}
 
@@ -308,7 +309,7 @@ REGISTRATION_MODERATION = True
 CSRF_COOKIE_NAME = 'varify_csrftoken'
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
-SESSION_COOKIE_AGE = 60 * 60 # 1 hour
+SESSION_COOKIE_AGE = 60 * 60  # 1 hour
 SESSION_COOKIE_NAME = 'varify_sessionid'
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_SAVE_EVERY_REQUEST = False
