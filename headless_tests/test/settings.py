8facc9adbdc7f4b17b964980f4d7b95a278659c8
Le Mar Davidson <davidsonl2@email.chop.edu>
2014-04-08 10:46:04 -0400

    Updating settings

--- headless_tests/test/settings.py
+++ headless_tests/test/settings.py
@@ -1,7 +1,1 @@
-#Settings for running headless_tests
-DEVELOPMENT_SERVER_HOST='localhost'
-#DEVELOPMENT_SERVER_PORT=8004
-DEVELOPMENT_SERVER_PORT=8060
-PHANTOMJS_PORT=8150
-PHANTOMJS_HOST='resrhtiuws03.research.chop.edu'
-LOG_FILE='/home/user001/log/project_name_phantomjs.log'
+
