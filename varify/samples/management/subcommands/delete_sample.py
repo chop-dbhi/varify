cd34da2bcb453283315a0de8bd1ea16a8336e788
Don Naegely <naegelyd@gmail.com>
2014-04-18 16:07:17 -0400

    Update delete-samples command to take multiple ids
    The ids are simply arguments after the command, each separated by a space.
    
    Signed-off-by: Don Naegely <naegelyd@gmail.com>

--- varify/samples/management/subcommands/delete_sample.py
+++ varify/samples/management/subcommands/delete_sample.py
@@ -19,55 +19,65 @@ class Command(BaseCommand):
         if not args:
             raise CommandError('A sample ID must be specified')
 
-        sample_id = args[0]
-
-        if Assessment.objects.filter(
-                sample_result__sample_id=sample_id).exists():
-            print("This sample cannot be deleted because it has knowledge "
-                  "capture data associated with it. If you are absolutely "
-                  "sure you want to delete this sample, manually delete the "
-                  "knowledge capture data for this sample and then rerun this "
-                  "command.")
-            return
-
         database = options.get('database')
         cursor = connections[database].cursor()
+
+        assessments = Assessment.objects.filter(
+            sample_result__sample_id__in=args)
+
+        invalid_ids = []
+        if assessments.exists():
+            invalid_ids = assessments.values_list('id', flat=True)
+            log.warning("Sample(s) with id '{0}' cannot be deleted "
+                        "because they have knowledge capture data "
+                        "associated with them. If you are absolutely "
+                        "sure you want to delete these samples, "
+                        "manually delete the knowledge capture data "
+                        "for them and then rerun this command."
+                        .format(", ".join([str(id) for id in invalid_ids])))
+
+        valid_ids = [id for id in args if id not in invalid_ids]
+        columns = ", ".join(['%s' for id in valid_ids])
+
+        if not valid_ids:
+            return
 
         with transaction.commit_manually(database):
             try:
                 # Remove sample from cohorts
                 cursor.execute('''
-                    DELETE FROM cohort_sample WHERE sample_id = %s
-                ''', [sample_id])
+                    DELETE FROM cohort_sample WHERE sample_id IN ({0})
+                '''.format(columns), valid_ids)
 
                 # Remove result scores
                 cursor.execute('''
                     DELETE FROM result_score WHERE result_id IN
-                        (SELECT id from sample_result WHERE sample_id = %s)
-                ''', [sample_id])
+                        (SELECT id from sample_result WHERE sample_id IN ({0}))
+                '''.format(columns), valid_ids)
 
                 # Remove results of sample
                 cursor.execute('''
-                    DELETE FROM sample_result WHERE sample_id = %s
-                ''', [sample_id])
+                    DELETE FROM sample_result WHERE sample_id IN ({0})
+                '''.format(columns), valid_ids)
 
                 # Remove runs from sample
                 cursor.execute('''
-                    DELETE FROM sample_run WHERE sample_id = %s
-                ''', [sample_id])
+                    DELETE FROM sample_run WHERE sample_id IN ({0})
+                '''.format(columns), valid_ids)
 
                 # Remove sample manifest
                 cursor.execute('''
-                    DELETE FROM sample_manifest WHERE sample_id = %s
-                ''', [sample_id])
+                    DELETE FROM sample_manifest WHERE sample_id IN ({0})
+                '''.format(columns), valid_ids)
 
                 # Remove sample
                 cursor.execute('''
-                    DELETE FROM sample WHERE sample.id = %s
-                ''', [sample_id])
+                    DELETE FROM sample WHERE sample.id IN ({0})
+                '''.format(columns), valid_ids)
 
                 transaction.commit()
             except DatabaseError:
                 transaction.rollback()
-                log.exception("Error occurred while deleting sample")
+                log.exception("Error occurred while deleting sample(s) with "
+                              "id '{0}'".format(valid_ids))
                 raise
