14f12addbd9a7cef4898be778a0720c4637bb12c
Byron Ruth <b@devel.io>
2014-04-29 11:52:26 -0400

    Implement SampleDialog for selecting a sample
    The dialog lists samples available to the user in a table which is
    sortable and can be filtered. Clicking a row will select the sample
    which then can be saved.
    
    Signed-off-by: Byron Ruth <b@devel.io>

--- varify/static/js/src/ui/templates.js
+++ varify/static/js/src/ui/templates.js
@@ -5,22 +5,28 @@ define([
     'tpl!../../../templates/tables/header.html',
     'tpl!../../../templates/modals/result.html',
     'tpl!../../../templates/modals/phenotypes.html',
+    'tpl!../../../templates/modals/sample.html',
     'tpl!../../../templates/controls/sift.html',
     'tpl!../../../templates/controls/polyphen.html',
     'tpl!../../../templates/workflows/results.html',
     'tpl!../../../templates/export/dialog.html',
-    'tpl!../../../templates/sample/loader.html'
-], function(c, header, result, phenotype, sift, polyphen, results,
-            exportDialog, sampleLoader) {
+    'tpl!../../../templates/sample/loader.html',
+    'tpl!../../../templates/sample/table.html',
+    'tpl!../../../templates/sample/row.html'
+], function(c, header, result, phenotype, sample, sift, polyphen, results,
+            exportDialog, sampleLoader, sampleTable, sampleRow) {
 
     // Define custom templates
     c.templates.set('varify/export/dialog', exportDialog);
     c.templates.set('varify/tables/header', header);
     c.templates.set('varify/modals/result', result);
     c.templates.set('varify/modals/phenotype', phenotype);
+    c.templates.set('varify/modals/sample', sample);
     c.templates.set('varify/controls/sift', sift);
     c.templates.set('varify/controls/polyphen', polyphen);
     c.templates.set('varify/workflows/results', results);
     c.templates.set('varify/sample/loader', sampleLoader);
+    c.templates.set('varify/sample/table', sampleTable);
+    c.templates.set('varify/sample/row', sampleRow);
 
 });
