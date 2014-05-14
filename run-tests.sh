50a355a39328aea0fedad83682fbc3a1af874dbf
Don Naegely <naegelyd@gmail.com>
2014-05-14 13:01:23 -0400

    Remove run-tests.sh, test_suite.py is preferred
    Signed-off-by: Don Naegely <naegelyd@gmail.com>

--- run-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ARGS="$@"
-
-if [ ! $ARGS ]; then
-    ARGS="assessments sample_load_process geneset_form south_tests commands"
-fi
-
-DJANGO_SETTINGS_MODULE='tests.settings' PYTHONPATH=. `which django-admin.py` test $ARGS
