ff54a6fccd9bce38e9a246dbb0068c67f51289c3
Don Naegely <naegelyd@gmail.com>
2014-10-24 11:38:44 -0400

    Remove reference to Genome model in genome admin
    This model has been removed in VDW.
    
    Signed-off-by: Don Naegely <naegelyd@gmail.com>

--- varify/genome/admin.py
+++ varify/genome/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from vdw.genome.models import Genome, Genotype
+from vdw.genome.models import Genotype
 
 
-admin.site.register(Genome)
 admin.site.register(Genotype)
